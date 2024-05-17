e41ca9b43fb7094d41d5eeb755d10c51e1c984e2
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-05-17 23:14:48 +0000

    Added a navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../img/chef-dojo-logo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light pt-3 pb-3">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src={Logo} />
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn button-default rounded-pill me-3">Log In</button>
+					</Link>
+					<Link to="/demo">
+						<button className="btn button-accent rounded-pill">Sign Up</button>
 					</Link>
 				</div>
 			</div>
