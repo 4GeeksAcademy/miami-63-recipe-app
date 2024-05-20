6caad70de220b3c3e0251d2fd649daee85515237
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-05-20 21:57:37 +0000

    Still working on search bar

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -5,6 +5,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Single } from "./pages/single";
 import { UserHome } from "./pages/user-home";
+import { Search } from "./pages/search";
 // import { Signup } from "./pages/signup";
 // import { Login } from "./pages/login";
 import injectContext from "./store/appContext";
@@ -28,6 +29,7 @@ const Layout = () => {
                     <Route element={<Home />} path="/" />
                     <Route element={<UserHome />} path="/user-home" />
                     <Route element={<Single />} path="/single/:theid" />
+                    <Route element={<Search />} path="/search" />
                     {/* <Route element={<Signup />} path="/signup" /> */}
                     {/* <Route element={<Login />} path="/login" /> */}
                     <Route element={<h1>Not found!</h1>} />
