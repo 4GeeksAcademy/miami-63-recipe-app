89cea6bbeafc38fb1a651ffbfd50c15fb058b62e
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-05-21 13:51:00 +0000

    Made styling changes to the search bar

--- src/front/js/pages/user-home.js
+++ src/front/js/pages/user-home.js
@@ -1,29 +1,22 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import SearchBG from "../../img/steak-image.png";
 import "../../styles/user-home.css";
 
-import { Carousel } from "../component/carousel";
-
-export const userHome = () => {
+export const UserHome = () => {
     const { store, actions } = useContext(Context);
 
     return (
         <>
-            <Carousel />
-
-            <div className="container text-center">
-
-                <div className="mb-5">
-                    <h1 className="home-title mb-3">
-                        The best place<br />
-                        to store your recipes!
-                    </h1>
-                    <p className="home-blurp">Chef-Dojo is the ultimate app for storing, organizing, and sharing your culinary creations. Whether you are a seasoned chef or just starting out in the kitchen, Chef-Dojo makes it easy to keep all your recipes in one place.</p>
-                </div>
-
-                <div className="">
-                    <Link to={"/signup"} className="btn button-accent rounded-pill fs-5 ps-4 pe-4 pt-3 pb-3">Get Started</Link>
+            <div className="container-fluid d-flex justify-content-center search-section" style={{backgroundImage: `url(${SearchBG})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+                <div className="d-flex align-items-center justify-content-center col-4">
+                    <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" />
+                    <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </>
