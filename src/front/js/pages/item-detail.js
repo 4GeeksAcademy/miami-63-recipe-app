e33d14d43e784d5a9d6d70fb70eec0549af05204
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-05-23 19:56:40 +0000

    Started the item description page

--- src/front/js/pages/item-detail.js
+++ src/front/js/pages/item-detail.js
@@ -7,10 +7,56 @@ import "../../styles/user-home.css";
 export const ItemDetail = () => {
     const { store, actions } = useContext(Context);
 
+    const column_1 = "col-10";
+    const text_right = "text-end";
+
     return (
         <>
-            <div className="container d-flex justify-content-center">
-                <h1>This is the Item Details page</h1>
+            <div className="container">
+                <h1 className="mb-4">Nutrition Facts</h1>
+                <h3 className="mb-4">Banana</h3>
+
+                <table class="table table-striped">
+                    <thead class="table-dark">
+                        <tr>
+                            <th className={column_1}>Elements</th>
+                            <th className={text_right}>Amount</th>
+                            <th className={text_right}>Unit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td >Total Fat</td>
+                            <td className={text_right}>4.8</td>
+                            <td className={text_right}>g</td>
+                        </tr>
+                        <tr>
+                            <td>Cholesterol</td>
+                            <td className={text_right}>165</td>
+                            <td className={text_right}>mg</td>
+                        </tr>
+                        <tr>
+                            <td>Sodium</td>
+                            <td className={text_right}>71</td>
+                            <td className={text_right}>mg</td>
+                        </tr>
+                        <tr>
+                            <td>Carbohydrates</td>
+                            <td className={text_right}>71</td>
+                            <td className={text_right}>mg</td>
+                        </tr>
+                        <tr>
+                            <td>Fiber</td>
+                            <td className={text_right}>71</td>
+                            <td className={text_right}>mg</td>
+                        </tr>
+                        <tr>
+                            <td>Protein</td>
+                            <td className={text_right}>71</td>
+                            <td className={text_right}>mg</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     );
