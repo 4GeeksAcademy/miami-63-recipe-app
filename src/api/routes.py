c7324fea303d5704aefa375eaee460742c50708e
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-05-29 23:48:47 +0000

    Almost done

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,6 +3,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
+from flask_jwt_extended import jwt_required
+
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
