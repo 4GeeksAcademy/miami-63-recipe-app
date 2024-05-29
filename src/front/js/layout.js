86ee29afb27f7c2ffeac3e2f391baac2d0901b80
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-05-29 18:52:08 +0000

    Added a video

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
@@ -14,23 +14,17 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 
-//create your first component
-const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
-
-    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+const LayoutContent = () => {
+    const location = useLocation();
+    const renderNavbar = !["/login", "/signup"].includes(location.pathname);
 
     return (
         <div>
             <BrowserRouter basename={basename}>
-                <Navbar />
+            {renderNavbar && <Navbar />}
                 <Routes>
                     <Route element={<Home />} path="/" />
                     <Route element={<UserHome />} path="/user-home" />
-                    <Route element={<Single />} path="/single/:theid" />
-                    <Route element={<CreateRecipe />} path="/createrecipe" />
                     <Route element={<ItemDetail />} path="/item-detail/:id" />
                     <Route element={<Search />} path="/search" />
                     <Route element={<Signup />} path="/signup" />
@@ -39,6 +33,18 @@ const Layout = () => {
                 </Routes>
             </BrowserRouter>
         </div>
+    );
+};
+
+const Layout = () => {
+    const basename = process.env.BASENAME || "";
+
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
+
+    return (
+        <BrowserRouter basename={basename}>
+            <LayoutContent />
+        </BrowserRouter>
     );
 };
 
