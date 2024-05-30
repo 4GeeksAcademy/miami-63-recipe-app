8647d6d9ed56875f424dc9034232bf172d31c7db
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-05-30 00:37:43 +0000

    Hope we are getting close

--- src/front/js/pages/user-home.js
+++ src/front/js/pages/user-home.js
@@ -44,8 +44,19 @@ export const UserHome = () => {
 
     return (
         <>
-            <div className="container-fluid d-flex justify-content-center search-section mb-5" style={{ backgroundImage: `url(${SearchBG})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
-                {/* ... Existing search input and button ... */}
+           <div className="container-fluid d-flex justify-content-center search-section mb-5" style={{backgroundImage: `url(${SearchBG})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+                <div className="d-flex align-items-center justify-content-center col-4">
+                    <input className="search-query-input" placeholder="Search Ingredient" value={search} onChange={(e) => setSearch(e.target.value)} onKeyPress={(element) => {
+						if (element.key === "Enter") {
+							handleSearch();
+						};
+					}} />
+                    <button className="search-query-submit" type="submit" onClick={handleSearch}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
 
             <div className="container d-flex justify-content-end mb-4">
