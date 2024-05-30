8895492470432a55188c4432cf1dabbcc9887839
Jorge-Adnel Martin <Repartomoro@hotmail.com>
2024-05-30 01:12:11 +0000

    Created the fullstack authentication

--- src/api/models.py
+++ src/api/models.py
@@ -9,19 +9,17 @@ from sqlalchemy import create_engine
 db = SQLAlchemy()
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
-    
+
     def serialize(self):
         return {
-            "user_id": self.user_id,
+            "id": self.id,
             "email": self.email,
-            "password": self.password
             # do not serialize the password, its a security breach
         }
 
